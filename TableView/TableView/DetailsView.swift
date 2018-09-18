fd970e5a7b6f196ed4f555fe3088b9f75ecead18
Lorena <lorena.rodrigues@zup.com.br>
2018-09-18 15:17:02 -0300

    Versão 2 App Table View

--- TableView/TableView/DetailsView.swift
+++ TableView/TableView/DetailsView.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class DetailsView: UIViewController {
 
+    var selectedTask : Task?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
 
@@ -37,76 +38,31 @@ class DetailsView: UIViewController {
     
     @IBOutlet weak var descricaoLabel: UILabel!
     
-    class Task : ViewController.Task {
-//        var id : Int = 0
-//        var nomeTarefa : String = ""
-//        var data : Date = Date()
-//        var descricao : String = ""
-        
-//        init(nomeTarefa: String, data: String, descricao: String) {
-//            self.id += 1
-//            self.nomeTarefa = nomeTarefa
-//            self.descricao = descricao
-//            let dateFormatter = DateFormatter()
-//            dateFormatter.dateFormat = "dd-MM-yyyy"
-//            self.data =  dateFormatter.date(from: data)!
-//
-//        }
-        override init(nomeTarefa: String, data: String, descricao: String) {
-            super.init(nomeTarefa: nomeTarefa, data: data, descricao: descricao)
-        }
-        
-    }
-    
-    
-    var nomeTarefa = String()
-    var dataTarefa = Date()
-    var descricaoTarefa = String()
-    var selectedTask : Task?
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tarefaLabel.text = selectedTask?.nomeTarefa
-        datePicker.date = (selectedTask?.data)!
-        descricaoLabel.text = selectedTask?.descricao
-//        tarefaLabel.text = nomeTarefa
-//        datePicker.date = dataTarefa
-//        descricaoLabel.text = descricaoTarefa
+        tarefaLabel.text = self.selectedTask?.nomeTarefa
+        datePicker.date = (self.selectedTask?.data)!
+        descricaoLabel.text = self.selectedTask?.descricao
         
     }
     
     @IBAction func buttonExcluir(_ sender: Any) {
-//        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//            if segue.identifier == "SegueEdita"
-//            {
-//                if let destinationVC = segue.destination as? ViewController {
-//                    let indexPath = destinationVC.tableView.indexPathForSelectedRow
-//                    destinationVC.tasks.remove(at: (indexPath?.row)!)
-//                    destinationVC.tableView.reloadData()
-//                }
-//            }
-//        }
+
     }
-    
     
     @IBOutlet weak var textFieldTarefa: UITextField!
     
     @IBOutlet weak var textFieldDescricao: UITextField!
     
     @IBAction func buttonSalvar(_ sender: Any) {
-//        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//            if segue.identifier == "SegueEdita"
-//            {
-//                if let destinationVC = segue.destination as? ViewController {
-//                    let indexPath = destinationVC.tableView.indexPathForSelectedRow
-//                    destinationVC.tasks[(indexPath?.row)!].nomeTarefa = textFieldTarefa.text!
-//                    destinationVC.tasks[(indexPath?.row)!].data = datePicker.date
-//                    destinationVC.tasks[(indexPath?.row)!].descricao = textFieldDescricao.text!
-//                    destinationVC.tableView.reloadData()
-//                }
-//            }
-//        }
+        self.selectedTask?.descricao = textFieldDescricao.text!
+        self.selectedTask?.nomeTarefa = textFieldTarefa.text!
+        self.selectedTask?.data = datePicker.date
+        
     }
+    
+
     
     
     
--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -8,40 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
 
-    @IBAction func TextTarefa(_ sender: Any) {
-    }
-    
-    @IBAction func TextDescricao(_ sender: Any) {
-    }
-    
-    class Task {
-        var id : Int = 0
-        var nomeTarefa : String = ""
-        var data : Date = Date()
-        var descricao : String = ""
-        
-        init(nomeTarefa: String, data: String, descricao: String) {
-            self.id += 1
-            self.nomeTarefa = nomeTarefa
-            self.descricao = descricao
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "dd-MM-yyyy"
-            self.data =  dateFormatter.date(from: data)!
-            
-            
-        }
-    }
     
     var tasks = [Task]()
     var selectedTask : Task?
     func adicionaTarefas() {
-        tasks.append(Task(nomeTarefa: "Ir ao supermercado", data:"21-09-2018", descricao: "Ir ao supermercado Carrefour a noite"))
-        tasks.append(Task(nomeTarefa: "Ir ao médico", data:"15-09-2018", descricao: "Médico de vista" ))
-        tasks.append(Task(nomeTarefa: "Ir à academia", data: "05-10-2018", descricao: "Treino de perna"))
+        tasks.append(Task(nomeTarefa: "Ir ao supermercado", stringData:"21-09-2018", descricao: "Ir ao supermercado Carrefour a noite"))
+        tasks.append(Task(nomeTarefa: "Ir ao médico", stringData:"15-09-2018", descricao: "Médico de vista" ))
+        tasks.append(Task(nomeTarefa: "Ir à academia", stringData: "05-10-2018", descricao: "Treino de perna"))
     }
     
 
@@ -69,42 +46,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "dd-MM-yyyy"
             cell.dataTask.text = dateFormatter.string(from: tasks[indexPath.row].data)
+            tasks[indexPath.row].id = indexPath.row
         }
-        
-        
+
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        self.selectedTask = tasks[indexPath.row]
-        self.performSegue(withIdentifier: "ShowTaskSegue", sender: nil)
-
-    }
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        self.selectedTask = tasks[indexPath.row]
+//        self.performSegue(withIdentifier: "ShowTaskSegue", sender: nil)
+//
+//    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowTaskSegue"
         {
             if let destinationVC = segue.destination as? DetailsView {
-//               let indexPath = self.tableView.indexPathForSelectedRow
-//                destinationVC.nomeTarefa = self.tasks[(indexPath?.row)!].nomeTarefa
-//                destinationVC.dataTarefa = self.tasks[(indexPath?.row)!].data
-//                destinationVC.descricaoTarefa = self.tasks[(indexPath?.row)!].descricao
-                destinationVC.selectedTask = self.selectedTask as? DetailsView.Task
+               let indexPath = self.tableView.indexPathForSelectedRow
+                  self.selectedTask = self.tasks[(indexPath?.row)!]
+                  destinationVC.selectedTask = self.selectedTask
             }
         }
         
-//        if segue.identifier == "SegueCriar"
-//        {
-//            if let destinationVC = segue.destination as? DetailsView {
-//                let indexPath = self.tableView.indexPathForSelectedRow
-//                destinationVC.nomeTarefa = self.tasks[(indexPath?.row)!].nomeTarefa
-//                destinationVC.dataTarefa = self.tasks[(indexPath?.row)!].data
-//                destinationVC.descricaoTarefa = self.tasks[(indexPath?.row)!].descricao
-//
-//
-//           }
-//
-//        }
+        if segue.identifier == "SegueCriar"
+        {
+            if let destinationVC = segue.destination as? DetailsView {
+                //let indexPath = self.tableView.indexPathForSelectedRow
+                destinationVC.selectedTask?.nomeTarefa = String()
+                destinationVC.selectedTask?.stringData = Date().description
+                destinationVC.selectedTask?.descricao = String()
+
+
+           }
+
+        }
 
         
         
@@ -113,10 +88,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   
     
     @IBAction func buttonCriar(_ sender: Any) {
-        self.selectedTask = nil
-        performSegue(withIdentifier: "SegueCriacao", sender: nil)
-       }
-        
+//        self.selectedTask = nil
+//        performSegue(withIdentifier: "SegueCriar", sender: nil)
+     }
+    
         
     
 
